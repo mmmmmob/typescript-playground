4456b12b3f8a0f0b2d73b92e379da5aab6acf084
Theppitak M. <141404845+mmmmmob@users.noreply.github.com>
2024-07-06 13:45:44 +0700

    add: type annotation examples

---
+++ _Udemy/TypeScript_TCDG/features/annotations/variables.ts
@@ -0,0 +1,25 @@
+// Variables type annotation
+let apples: Number = 5;
+let speed: String = "fast";
+let hasName: Boolean = true;
+
+let nothingMuch: null = null;
+let nothing: undefined = undefined;
+
+// Built-in objects
+let now: Date = new Date();
+
+// Array
+let colors: String[] = ["red", "green", "blue"];
+let myNumbers: Number[] = [1, 2, 3];
+let truths: Boolean[] = [true, true, false];
+
+// Classes
+class Car {}
+let car: Car = new Car();
+
+// Object Literal
+let point: { x: Number; y: Number } = {
+  x: 10,
+  y: 20,
+};
