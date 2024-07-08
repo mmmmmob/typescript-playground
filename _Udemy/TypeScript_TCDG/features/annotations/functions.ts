3296c557350dd6e6bd246eaf3542406274ffba1e
Theppitak M. <141404845+mmmmmob@users.noreply.github.com>
2024-07-08 18:45:26 +0700

    add: other example of type annotation on functions

--- _Udemy/TypeScript_TCDG/features/annotations/functions.ts
+++ _Udemy/TypeScript_TCDG/features/annotations/functions.ts
@@ -1,4 +1,4 @@
-// always explicitly annotate type to function arguments!
+// !! always explicitly annotate type to function arguments!
 // ----------------
 // Arrow functions
 const add = (a: number, b: number): number => {
@@ -18,3 +18,40 @@ function divide(a: number, b: number): number {
 const multiply = function (a: number, b: number): number {
   return a * b;
 };
+
+// function that doesn't return any value (void type)
+const logger = (message: string): void => {
+  console.log(message);
+};
+
+// 'never' type represents values that never occur
+const throwError = (message: string): never => {
+  throw new Error(message);
+};
+
+// above case can't reach the end of the function compared to this case so we annotate string type on return value
+const optionalThrowError = (message: string): string => {
+  if (!message) {
+    throw new Error(message);
+  }
+  return message;
+};
+
+// use destructuring syntax with object
+const todaysWeather = {
+  date: new Date(),
+  weather: "Today's Sunny",
+};
+
+const logWeather = ({
+  date,
+  weather,
+}: {
+  date: Date;
+  weather: string;
+}): void => {
+  console.log(date);
+  console.log(weather);
+};
+
+logWeather(todaysWeather);
