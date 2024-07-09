0280a5b4a8ee2289c4ec6ae9128894150139dd16
Theppitak M. <141404845+mmmmmob@users.noreply.github.com>
2024-07-10 00:07:39 +0700

    add: more example of types on array

--- _Udemy/TypeScript_TCDG/features/arrays.ts
+++ _Udemy/TypeScript_TCDG/features/arrays.ts
@@ -1,9 +1,28 @@
 // always annotate array type when init an empty array
 const carMakers: string[] = [];
 carMakers.push("Toyota");
+carMakers.push("Ford");
+carMakers.push("Rolls-Royce");
 
 // if not, we can use the type inference feature of TypeScript
 const dates = [new Date(), new Date(), new Date()];
 
 // 2D array will be annotate with /type[][]/
 const carsByMake: string[][] = [["Corolla"]];
+
+// TypeScript help inference the type when access array value
+const car = carMakers[0];
+const myCar = carMakers.pop();
+
+// Also helps in preventing incompatible value
+carMakers.push(10); // this results in error
+
+// Get help with array built-in methods
+const uppercasedCar = carMakers.map((car: string): string => {
+  return car.toUpperCase();
+}); // [ "TOYOTA", "FORD" ]
+
+// Can use and check multiple types within an array
+const importantDates: (Date | string)[] = [new Date()];
+importantDates.push("2024-07-10");
+importantDates.push(10); // this results in error
