58e632de9d1940aee743f6dd0a16d80110c50cf7
Theppitak M. <141404845+mmmmmob@users.noreply.github.com>
2024-07-11 00:03:20 +0700

    chore: refactor existing interface

--- _Udemy/TypeScript_TCDG/features/interfaces.ts
+++ _Udemy/TypeScript_TCDG/features/interfaces.ts
@@ -1,12 +1,16 @@
-interface Vehicle {
+interface Reportable {
+  /*
   name: string;
   year: Date;
   isBroken: boolean;
+  */
+  // Reportable interface only check if the object passed in satisfied the rule or not?
   summary(): string; // function that expect to return a string
 }
 
 // this object satisfied the Vehicle interface rules
 const oldCivic = {
+  // we can insert more value into the object and it's still satisfied the interface rule
   name: "Civic",
   year: new Date(),
   isBroken: true,
@@ -19,8 +23,8 @@ const oldCivic = {
   },
 };
 
-const printVehicle = (vehicle: Vehicle): void => {
-  console.log(vehicle.summary());
+const printSummary = (item: Reportable): void => {
+  console.log(item.summary());
 };
 
-printVehicle(oldCivic);
+printSummary(oldCivic);
