522b8769dac5dd2cd8f022e7e573fe8b5d566f7f
Theppitak M. <141404845+mmmmmob@users.noreply.github.com>
2024-07-11 22:49:58 +0700

    add: example code for privacy modifiers

--- _Udemy/TypeScript_TCDG/features/classes.ts
+++ _Udemy/TypeScript_TCDG/features/classes.ts
@@ -1,26 +1,40 @@
 // create a blueprint
 class Vehicle {
-  drive(): void {
+  // * modifier: public = as a default access control -> can access this method from anywhere at anytime
+  public drive(): void {
     console.log("STARTED THE ENGINE!");
   }
 
-  honk(): void {
+  // * modifier: protected = can access only within its class and classes that extends (can't be called outside)
+  protected honk(): void {
     console.log("BEEP!");
   }
 }
 
-// /extends/ syntax will inherit all methods and values from existing class to new one
+// * /extends/ syntax will inherit all methods and values from existing class to new one
 class Car extends Vehicle {
-  // /override/ syntax make sure to check if the method we extended and want to overridden exists
+  // * /override/ syntax make sure to check if the method we extended and want to overridden exists
   override drive(): void {
     console.log("CAR ENGINE STARTED!");
+  }
+
+  // * modifier: private = can be accessed within its class only
+  private accelerate(): void {
+    this.honk();
+    console.log("GO STRAIGHT AT 60MPH.");
+  }
+
+  startDrivingProcess(): void {
+    this.drive();
+    this.honk(); // <- this can be used because the parent class marked it as 'protected'
+    this.accelerate();
   }
 }
 
 // create new instance from the blueprint with /new/ syntax
 const vehicle = new Vehicle();
 vehicle.drive();
-vehicle.honk();
+vehicle.honk(); // <- this resulted in error
 
 const car = new Car();
-car.drive();
+car.startDrivingProcess();
