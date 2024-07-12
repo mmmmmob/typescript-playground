117db3bdb3dbf46322d44d461d7ae1748a6c4f64
Theppitak M. <141404845+mmmmmob@users.noreply.github.com>
2024-07-12 20:28:57 +0700

    feat: display maps on screen

--- _Udemy/TypeScript_TCDG/maps/src/index.ts
+++ _Udemy/TypeScript_TCDG/maps/src/index.ts
@@ -2,7 +2,10 @@
 import { Company } from "./Company";
 import { User } from "./User";
 
-const user = new User();
-const company = new Company();
-console.log(user);
-console.log(company);
+new google.maps.Map(document.getElementById("maps") as HTMLElement, {
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0,
+  },
+});
