9f5db5407139fd07a8922d977351380b75fa21c7
Theppitak M. <141404845+mmmmmob@users.noreply.github.com>
2024-07-14 16:21:40 +0700

    feat: add marker for company and user on the map

--- _Udemy/TypeScript_TCDG/maps/src/CustomMap.ts
+++ _Udemy/TypeScript_TCDG/maps/src/CustomMap.ts
@@ -1,5 +1,6 @@
 /// <reference types="@types/google.maps" />
-
+import { Company } from "./Company";
+import { User } from "./User";
 export class CustomMap {
   // to limit access of Google Maps method with /private/ syntax
   private googleMap: google.maps.Map;
@@ -16,5 +17,25 @@ export class CustomMap {
         },
       }
     );
+  }
+
+  addUserMarker(user: User): void {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: user.location.lat,
+        lng: user.location.lng,
+      },
+    });
+  }
+
+  addCompanyMarker(company: Company): void {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: company.location.lat,
+        lng: company.location.lng,
+      },
+    });
   }
 }
--- _Udemy/TypeScript_TCDG/maps/src/index.ts
+++ _Udemy/TypeScript_TCDG/maps/src/index.ts
@@ -3,3 +3,8 @@ import { CustomMap } from "./CustomMap";
 import { User } from "./User";
 
 const customMap = new CustomMap("maps");
+const user = new User();
+const company = new Company();
+
+customMap.addUserMarker(user);
+customMap.addCompanyMarker(company);
