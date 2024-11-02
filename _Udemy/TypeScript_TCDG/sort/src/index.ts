e75a198bf91973f058116aad543fe56a378b6200
Theppitak M. <141404845+mmmmmob@users.noreply.github.com>
2024-11-02 19:02:35 +0700

    add: implements sort with linked list

--- _Udemy/TypeScript_TCDG/sort/src/index.ts
+++ _Udemy/TypeScript_TCDG/sort/src/index.ts
@@ -1,14 +1,25 @@
 import { CharacterCollection } from "./CharacterCollection";
+import { LinkedList } from "./LinkedList";
 import { NumberCollection } from "./NumbersCollection";
 import { Sorter } from "./Sorter";
 
-const numberCollection = new NumberCollection([-10, 3, -4, 0]);
-const characterCollection = new CharacterCollection("cdAGhf");
+// const numberCollection = new NumberCollection([-10, 3, -4, 0]);
+// const characterCollection = new CharacterCollection("cdAGhf");
 
-const sorterNum = new Sorter(numberCollection);
-sorterNum.sort();
-const sorterChar = new Sorter(characterCollection);
-sorterChar.sort();
+// const sorterNum = new Sorter(numberCollection);
+// sorterNum.sort();
+// const sorterChar = new Sorter(characterCollection);
+// sorterChar.sort();
 
-console.log(numberCollection.data);
-console.log(characterCollection.data);
+// console.log(numberCollection.data);
+// console.log(characterCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(5);
+
+const sorter = new Sorter(linkedList);
+sorter.sort();
+linkedList.print();
