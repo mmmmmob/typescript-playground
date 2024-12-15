603a175d3282c3791880a31dabc93e60188b51a8
Theppitak M. <141404845+mmmmmob@users.noreply.github.com>
2024-12-15 21:35:31 +0700

    refactor: using enum

--- _Udemy/TypeScript_TCDG/stats/build/index.js
+++ _Udemy/TypeScript_TCDG/stats/build/index.js
@@ -4,15 +4,24 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const fs_1 = __importDefault(require("fs"));
-const matches = fs_1.default.readFileSync("football.csv", { encoding: "utf-8", })
-    .split('\n')
-    .map((row) => { return row.split(','); });
+const matches = fs_1.default
+    .readFileSync("football.csv", { encoding: "utf-8" })
+    .split("\n")
+    .map((row) => {
+    return row.split(",");
+});
 let manUnitedWins = 0;
+var MatchResult;
+(function (MatchResult) {
+    MatchResult["HomeWin"] = "H";
+    MatchResult["AwayWin"] = "A";
+    MatchResult["Draw"] = "D";
+})(MatchResult || (MatchResult = {}));
 for (let match of matches) {
-    if (match[1] === 'Man United' && match[5] === 'H') {
+    if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
         manUnitedWins++;
     }
-    else if (match[2] === 'Man United' && match[5] === 'A') {
+    else if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
         manUnitedWins++;
     }
 }
--- _Udemy/TypeScript_TCDG/stats/src/index.ts
+++ _Udemy/TypeScript_TCDG/stats/src/index.ts
@@ -1,17 +1,26 @@
 import fs from "fs";
 
-const matches = fs.readFileSync("football.csv", {encoding: "utf-8",})
-    .split('\n')
-    .map((row: string): string[] => {return row.split(',')})
+const matches = fs
+  .readFileSync("football.csv", { encoding: "utf-8" })
+  .split("\n")
+  .map((row: string): string[] => {
+    return row.split(",");
+  });
 
-let manUnitedWins = 0
+let manUnitedWins = 0;
+
+enum MatchResult {
+  HomeWin = "H",
+  AwayWin = "A",
+  Draw = "D",
+}
 
 for (let match of matches) {
-    if (match[1] === 'Man United' && match[5] === 'H') {
-        manUnitedWins++
-    } else if (match[2] === 'Man United' && match[5] === 'A') {
-        manUnitedWins++
-    }
+  if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
+    manUnitedWins++;
+  } else if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
+    manUnitedWins++;
+  }
 }
 
 console.log(`Manchester United win: ${manUnitedWins} games`);
