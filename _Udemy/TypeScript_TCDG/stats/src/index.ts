c71c25ad8b96d55c4751dfbeaa6e3e9f8cceb5ad
Theppitak M. <141404845+mmmmmob@users.noreply.github.com>
2025-01-23 00:11:59 +0700

    feat: convert data in each row into specific tuples type

--- _Udemy/TypeScript_TCDG/stats/src/index.ts
+++ _Udemy/TypeScript_TCDG/stats/src/index.ts
@@ -1,17 +1,11 @@
-import fs from "fs";
 import { CsvFileReader } from "./CsvFileReader";
+import { MatchResult } from "./MatchResult";
 
 const reader = new CsvFileReader("football.csv");
 
 reader.read();
 
 let manUnitedWins = 0;
-
-enum MatchResult {
-  HomeWin = "H",
-  AwayWin = "A",
-  Draw = "D",
-}
 
 for (let match of reader.data) {
   if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
