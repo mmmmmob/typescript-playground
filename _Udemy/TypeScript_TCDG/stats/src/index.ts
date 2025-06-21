ab49f79cc3c939e7adbed79c39f8c1b80afd7fa9
Theppitak M. <141404845+mmmmmob@users.noreply.github.com>
2025-06-21 22:47:24 +0700

    feat: create html with fs

--- _Udemy/TypeScript_TCDG/stats/src/index.ts
+++ _Udemy/TypeScript_TCDG/stats/src/index.ts
@@ -2,12 +2,13 @@ import { WinAnalysis } from "./analyzers/WinAnalysis";
 import { CsvFileReader } from "./composition/CsvFileReader";
 import { MatchReader } from "./composition/MatchReader";
 import { ConsoleReport } from "./reportTargets/ConsoleReport";
+import { HtmlReport } from "./reportTargets/HtmlReport";
 import { Summary } from "./Summary";
 
 const csvReader = new CsvFileReader("football.csv");
 const matchReader = new MatchReader(csvReader);
 matchReader.load();
 
-const summary = new Summary(new WinAnalysis("Man United"), new ConsoleReport());
+const summary = new Summary(new WinAnalysis("Man United"), new HtmlReport());
 
 summary.buildAndPrintReport(matchReader.matches);
