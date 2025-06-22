40648c4dc2176c34e9d83cfd9fcbccb7a8dd7f89
Theppitak M. <141404845+mmmmmob@users.noreply.github.com>
2025-06-22 09:29:39 +0700

    add: generics const

--- _Udemy/TypeScript_TCDG/features/generics.ts
+++ _Udemy/TypeScript_TCDG/features/generics.ts
@@ -1,3 +1,4 @@
+// * generics on class
 class ArrayOfNumber {
   constructor(public collection: number[]) {}
   get(index: number): number {
@@ -12,7 +13,6 @@ class ArrayOfString {
   }
 }
 
-// ? recreate as generics class
 class ArrayOfAnything<T> {
   constructor(public collection: T[]) {}
   get(index: number): T {
@@ -21,4 +21,39 @@ class ArrayOfAnything<T> {
 }
 
 const example = new ArrayOfAnything<string>(["a", "b", "c"]);
-console.log(example.get(2)); // 'c'
+example.get(2); // 'c'
+
+// * generics on function
+const printAnything = <T>(arr: T[]): void => {
+  for (const member of arr) {
+    console.log(member);
+  }
+};
+
+// * generics on const
+
+class House {
+  print(): void {
+    console.log("I am a house");
+  }
+}
+
+class Car {
+  print(): void {
+    console.log("I am a car");
+  }
+}
+
+interface Printable {
+  print(): void;
+}
+
+// use extends to mark T align with Printable interface aka you better have .print() on hand
+function printHouseOrCar<T extends Printable>(arr: T[]) {
+  for (const member of arr) {
+    member.print();
+  }
+}
+
+printHouseOrCar([1, 2, 3, 4]); // error because number doesn't have .print() method
+printHouseOrCar<Car>([new Car(), new Car()]);
